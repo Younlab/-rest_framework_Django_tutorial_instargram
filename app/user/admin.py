8c7833c8ca0c9211a2735190cdac234050a92ce6
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-26 18:43:51 +0900

    User model create

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'username',
+                'password',
+            ),
+        }),
+        ('개인정보', {
+            'fields': (
+                'last_name',
+                'first_name',
+                'email',
+                'profile_image',
+                'site',
+            ),
+        }),
+        ('권한', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+        ('주요 일자', {
+            'fields': (
+                'last_login',
+                'date_joined',
+            ),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- app/user/models.py
+++ app/user/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    profile_image = models.ImageField(upload_to='pofile_image', blank=True)
+    site = models.URLField(blank=True)
+    email = models.EmailField(blank=True)
