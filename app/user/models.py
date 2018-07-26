1438b3e90b0a7ded1c2318bb84dff39886beee37
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-27 00:46:01 +0900

    user models create

--- app/user/models.py
+++ app/user/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     profile_image = models.ImageField(upload_to='pofile_image', blank=True)
     site = models.URLField(blank=True)
