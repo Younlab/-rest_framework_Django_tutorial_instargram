016c306d4c656130c2ad6cd2fd143497edd547bb
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-27 00:46:57 +0900

    sign_in, sign_up views create

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms.sign_up_form import SignUpForm
+
+
+def sign_in(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+    return render(request, 'users/sign_in.html')
+
+
+def sign_up(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.created_user()
+            user.save()
+            login(request, user)
+            return redirect('index')
+
+    context = {
+        'forms': form,
+    }
+
+    return render(request, 'users/sign_up.html', context)
